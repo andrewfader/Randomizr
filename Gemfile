08ba664535382b0877d8b6b66c581937ee2375dd
unknown <Andrew@.(none)>
2011-07-24 20:46:16 -0400

    new app

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rake', '0.8.7'
+gem 'rails', '3.0.9'
+gem 'bundler'
+gem 'carrierwave'
+gem 'compass'
+gem 'high_voltage'
+gem 'inherited_resources'
+gem 'jquery-rails'
+gem 'simple_form'
+gem 'sqlite3'
+gem 'nokogiri'
+gem 'devise'
+gem "bundler"
+gem "haml", ">= 3.0.0"
+gem "haml-rails"
+gem "simple_form"
+gem "inherited_resources"
+gem 'carrierwave'
+gem "cancan"
+gem "heroku"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Randomizr::Application.config.secret_token = 'fc33647027f9671aeff005122594b45b46e8cd48de43bfe12ca5544dbab116a27aa3f93dfb116334659060b35dc463f77248fc27748b3354e55ea8b48d6a22ed'
